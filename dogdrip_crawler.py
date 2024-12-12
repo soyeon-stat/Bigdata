8daf4a625b6af877523b4ff24f66eb6fbfe0e10c
SEUNGTO <tmdduq9008@gmail.com>
2024-12-12 23:19:00 +0900

    개드립 크롤러 최종버전 업데이트함

--- dogdrip_crawler.py
+++ dogdrip_crawler.py
@@ -1,3 +1,4 @@
+import pdb
 import pandas as pd
 import requests
 import time
@@ -6,13 +7,47 @@ import pytz
 import re
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
+from config.config import *
 
 # timezone 고정
 tz = pytz.timezone('Asia/Seoul')
 
+def upload_to_source_data(doc_id, data) :
+
+    url = f"{OPENSEARCH_URL}/source/_doc/{doc_id}"
+    response = requests.post(
+        url = url,
+        headers = OPENSEARCH_HEADERS,
+        auth = OPENSEARCH_AUTH,
+        data = json.dumps(data)
+    )
+
+    assert response.status_code >= 200 and response.status_code < 300
+
+def check_contain_keywords(doc, keyword) :
+    title = doc['title']
+    content = doc['content']
+    comments = ",".join(doc['comments'])
+    text = f"제목 : {title} \n 내용 : {content} \n 댓글 : {comments}"
+    for k in keyword :
+        if k in text :
+            return True
+    else :
+        return None
+
+def check_if_doc_existing(doc_id) :
+    url = f'{OPENSEARCH_URL}/source/_docs/{doc_id}'
+    resp = requests.get(
+        url = url, 
+        auth = OPENSEARCH_AUTH,
+    )
+
+    return resp.status_code == 200
+
 
 ## Default Setting
 def fetch_find_post_date(row, now):
+
     """
     게시판의 행을 읽어서 날짜를 추출하는 함수
     X시간 전으로 표기된 경우들을 처리함
@@ -24,10 +59,14 @@ def fetch_find_post_date(row, now):
     # 글머리에 해당하는 경우
     if len(row.find_all('th')) > 0:
         return None
-
+    
     # 글 등록시간
-    dates = row.find('td', {'class': 'time'}).text
-    if "분" in dates:
+    dates = row.find('span', {'class': 'ed text-muted text-xxsmall margin-right-xsmall'}).text.strip()
+
+    if "방금 전" in dates :
+        dates = now
+
+    elif "분" in dates:
         minutes = int(re.sub("\D+", "", dates))
         dates = now - timedelta(minutes=minutes)
 
@@ -62,24 +101,22 @@ def fetch_post_information(row, now, end_date) :
         # 사전에 정한 기간 (크롤링 시간으로부터 일주일)을 넘어서면 for문 중단
         if dates.strftime('%Y-%m-%d') < end_date :
             return None
-
+        
         # 글제목, 글번호
-        title_row = row.find('td', {'class': 'title'})
-        title = title_row.find('span', {'class': 'ed title-link'}).text
+        title_row = row.find('div', {'class': 'ed list-header'})
+        if title_row is None :
+            return None
+        title = title_row.find('a', {'class': 'ed title-link'}).text.strip()
 
         href = title_row.find('a')['href']
         post_no = href.split("?")[0].split("/")[-1]
 
         # 글쓴이
-        author = row.find('td', {'class': 'author'}).text.strip()
-
-        # 추천수
-        vote = row.find('td', {'class': 'ed voteNum text-primary'}).text
+        writer = row.find('span', {'class': 'ed text-xsmall inline-flex flex-left flex-middle margin-right-xsmall'}).text.strip()
 
         return {'post_no': post_no,
                  'title': title,
-                 'author': author,
-                 'vote': vote,
+                 'writer': writer,
                  'dates': dates
                  }
 
@@ -94,17 +131,20 @@ def fetch_board_list(page, days = 7) :
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
     }
-    url = f'https://www.dogdrip.net/dogdrip?sort_index=popular&page={page}'
+    url = f'https://www.dogdrip.net/dogdrip?sort_index=popular&page={page}' # 인기글만
+    # url = f'https://www.dogdrip.net/dogdrip?page={page}'
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
     board_list = soup.find('div', {'class' : 'ed board-list'})
+
 
     now = datetime.now(tz)
     end_date = pd.to_datetime(now - timedelta(days = days))
     end_date = end_date.strftime('%Y-%m-%d')
 
     post_data = pd.DataFrame({})
-    rows = board_list.find_all('tr')
+    rows = board_list.find_all('li', {'class' : 'ed flex flex-left flex-middle webzine'})
+
 
     try :
         for row in rows :
@@ -198,59 +238,80 @@ def fetch_post_content(post_no):
             continue
 
     # 댓글 수
-    n_comments = container.find('h4', {'class': 'ed comment-header'}).text.strip()
-    n_comments = int(re.sub("\D", "", n_comments))
+    n_comment = container.find('h4', {'class': 'ed comment-header'}).text.strip()
+    n_comment = int(re.sub("\D", "", n_comment))
 
     # 댓글 내용
     last_c_page = find_last_comment_page(soup)
     comments = fetch_comment_contents(post_no, last_c_page)
 
+    # 추천수/비추천수
+    script = soup.find('script',  {'type' : "text/javascript"}).text
+    up_pattern = r'<span class="num" id="document_voted_count">(\d+)</span>'
+    down_pattern = r'<span class="num" id="document_blamed_count">(-{0,1}\d+)</span>'
+
+    vote_up = re.findall(up_pattern, script)[0]
+    vote_down = re.findall(down_pattern, script)[0]
+
     return {'post_no':  post_no,
-            'comment':  n_comments,
+            'n_comment':  n_comment,
             'content':  body,
+            'vote_up' : vote_up,
+            'vote_down' : vote_down,
             'comments': comments,
-            'updated' : datetime.now().strftime('%Y-%m-%d %H:%M'),
+            'timestp' : datetime.now().strftime('%Y-%m-%d %H:%M'),
             }
 
 if __name__ == "__main__" :
 
     # 게시글 목록 리스팅
     page = 1
-    days = 14        # 크롤링할 기간(N일 전부터 오늘까지)
-    contents = []
+    days = 41        # 크롤링할 기간(N일 전부터 오늘까지)
+
+    keyword_list = ['계엄령', '대통령', '윤석열', '비트코인', '업비트', '흑백요리사', '로제', '이재명', '한강', '코스피', '백종원']
 
     while True :
 
         # 게시판 페이지 글 리스트업
         post = fetch_board_list(page, days)
+
         if post.shape[0] > 0 :
 
             for idx, v in post.iterrows() :
-                post_no, title, author, vote, dates = v
+                post_no, title, writer, dates = v
+
                 # 본문 & 댓글 크롤링
                 article_comments = fetch_post_content(post_no)
 
                 result = {
-                    'ID': f'dogdrip-{post_no}',
                     'community': '개드립넷',
-                    'link': f'https://www.dogdrip.net/dogdrip/{post_no}',
+                    'ID': f'dogdrip{post_no}',
                     'category': '개드립 인기글',
+                    'link': f'https://www.dogdrip.net/dogdrip/{post_no}',
                     'title': title,
-                    'vote': vote,
+                    'vote_up': article_comments['vote_up'],
+                    'vote_down':article_comments['vote_down'],
                     'view': None,
-                    'comment': article_comments['comment'],
-                    'date': dates.strftime('%Y-%m-%d %H:%M'),
+                    'writer' : writer,
+                    'n_comment': article_comments['n_comment'],
+                    'post_date': dates.strftime('%Y-%m-%d %H:%M'),
                     'content': article_comments['content'],
                     'comments': article_comments['comments'],
-                    'updated' : article_comments['updated']
+                    'timestp' : article_comments['timestp']
                 }
-                contents.append(result)
-                time.sleep(0.5)
+                print(page, result['timestp'], post_no, result['title'])
+                
+
+
+                if check_if_doc_existing(result['ID']) :
+                    continue
+
+                if check_contain_keywords(result, keyword_list):
+                    upload_to_source_data(result['ID'], result)
+                
+                time.sleep(0.1)
 
             page += 1
 
         else :
             break
-
-    with open("dogdrip_data.json", "w", encoding="utf-8") as json_file:
-        json.dump(contents, json_file, ensure_ascii=False, indent=4)
